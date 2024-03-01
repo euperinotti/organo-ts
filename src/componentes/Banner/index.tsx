1c229edf6c4a810bf1a08105a08fb039b063cfb8
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-03-01 19:11:24 -0300

    refactor: migrate banner to tsx

---
+++ src/componentes/Banner/index.tsx
@@ -0,0 +1,19 @@
+import "./Banner.css";
+
+interface BannerProps {
+  src: string;
+  alt?: string;
+}
+
+const Banner = ({ src, alt }: BannerProps) => {
+  return (
+    <header className="banner">
+      <img
+        src={src}
+        alt={alt}
+      />
+    </header>
+  );
+};
+
+export default Banner;
