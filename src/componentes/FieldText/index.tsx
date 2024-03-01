233e447495a68ce08613124539bd9a8751becf6e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-03-01 19:12:12 -0300

    refactor: migrate field text to tsx

---
+++ src/componentes/FieldText/index.tsx
@@ -0,0 +1,37 @@
+import "./FieldText.css";
+
+interface FieldTextProps {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  required: boolean;
+  placeholder: string;
+}
+
+const FieldText = ({
+  label,
+  value,
+  onChange,
+  required,
+  placeholder,
+}: FieldTextProps) => {
+  const newPlaceholder = `${placeholder}...`;
+
+  const handleInput = (evento: any) => {
+    onChange(evento.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input
+        value={value}
+        onChange={handleInput}
+        required={required}
+        placeholder={newPlaceholder}
+      />
+    </div>
+  );
+};
+
+export default FieldText;
